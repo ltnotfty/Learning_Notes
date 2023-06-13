f33d40bc1f3696bc350272ff6f1735d155ee4e58
ltnotfty <ltnotfty@126.com>
2023-06-13 14:51:42 +0800

    First commit

---
+++ simpleRpc/cmake-build-debug/CMakeFiles/simpleRpc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simpleRpc.dir/main.cpp.o"
+  "CMakeFiles/simpleRpc.dir/main.cpp.o.d"
+  "simpleRpc"
+  "simpleRpc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simpleRpc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ simpleRpc/cmake-build-debug/CMakeFiles/simpleRpc.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for simpleRpc.
+# This may be replaced when dependencies are built.
